469be042966827b464abee6c3706e233e833caac
Salim Ansari <salim.ansari@nw18.com>
2025-07-11 18:20:44 +0530

    add exp

--- src/app/components/Experience/index.tsx
+++ src/app/components/Experience/index.tsx
@@ -19,6 +19,7 @@ export default function Experience() {
             </a>
             <p className={styles.time}>June 2023 - Present</p>
           </div>
+          <h4 className={styles.desg}>Key Responsibilities & Technologies:</h4>
 
           <ul>
             <li className={styles.keyItem}>
@@ -65,6 +66,61 @@ export default function Experience() {
             </li>
             <li className={styles.techItem} style={{ color: '#af7aa1' }}>
               MongoDB
+            </li>
+          </ul>
+        </div>
+        <div className={styles.expItem}>
+          <h4 className={styles.desg}>Trainee</h4>
+          <div className={styles.compDur}>
+            <a
+              href="https://www.nw18.com/"
+              className={styles.compName}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Network18 Media & Investments Limited
+            </a>
+            <p className={styles.time}>June 2022 - July 2023</p>
+          </div>
+          <h4 className={styles.desg}>Key Responsibilities & Technologies:</h4>
+
+          <ul>
+            <li className={styles.keyItem}>
+              Developed interactive user interfaces using React.js and JavaScript, focusing on component-based 
+ architecture and state management.
+            </li>
+            <li className={styles.keyItem}>
+              Built server-side rendered applications and optimized routing with Next.js.
+            </li>
+            <li className={styles.keyItem}>
+             Created and managed backend APIs using Node.js and Express.js, integrating database operations 
+ and middleware.
+            </li>
+            
+          </ul>
+
+          <h4 className={styles.subttl}>Technologies</h4>
+          <ul>
+            <li className={styles.techItem} style={{ color: '#4e79a7' }}>
+              JavaScript
+            </li>
+            <li className={styles.techItem} style={{ color: '#f28e2b' }}>
+              React.js
+            </li>
+            <li className={styles.techItem} style={{ color: '#e15759' }}>
+              Next.js
+            </li>
+            <li className={styles.techItem} style={{ color: '#9c755f' }}>
+              Node.js
+            </li>
+            <li className={styles.techItem} style={{ color: '#59a14f' }}>
+              Express.js
+            </li>
+            <li className={styles.techItem} style={{ color: '#af7aa1' }}>
+              HTML
+            </li>
+            <li className={styles.techItem} style={{ color: '#9c755f' }}>
+              CSS
             </li>
           </ul>
         </div>
