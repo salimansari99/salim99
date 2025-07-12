fbe106f8ed06ccbe34ab736878071c4cc2f7f016
Salim Ansari <salim.ansari@nw18.com>
2025-07-12 15:05:39 +0530

    add project comp

--- src/app/components/Projects/index.tsx
+++ src/app/components/Projects/index.tsx
@@ -1,22 +1,35 @@
 'use client';
+import styles from './Projects.module.css';
+
 export default function Projects() {
     return (
-        <>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100..900;1,100..900&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&family=Yuji+Mai&display=swap"
-        rel="stylesheet"
-      />
-        <section className="projects-container">
-            <div className="coming-soon">
-                Coming Soon...
-            </div>
+        <section className={styles.projectsContainer}>
+            <h2 className={styles.title}>Experience</h2>
+            <ul className={styles.projectsList}>
+                <li className={styles.projectItem}>
+                    <div className={styles.projectTop}>
+                        {/* <img className={styles.suitcaseIc} src="/images/briefcase.png" alt="" /> */}
+                        <h4 className={styles.projectName}>The Thought Waves</h4>
+                    </div>
+                    <p className={styles.projectDesc}>
+                        Thought Waves – A Modern Blogging Platform for Insightful Minds
+                    </p>
+                    <ul className={styles.descList}>
+                        <li>✍️ Showcases well-researched articles on AI, technology, travel, and creativity.</li>
+                        <li>📱 Optimized for all devices with a clean and engaging UI.</li>
+                        <li>🧩 Features include blog listings, individual post pages, and popular posts section.</li>
+                    </ul>
+                    <ul className={styles.technologies}>
+                        <li className={styles.techItem}>ReactJS</li>
+                        <li className={styles.techItem}>NextJS</li>
+                        <li className={styles.techItem}>TypeScript</li>
+                    </ul>
+                    <div className={styles.demo}>
+                        <img className={styles.linkIc} src="/images/link.png" alt="Link" />
+                        <a href="https://thought-waves.vercel.app/" className={styles.demoLink}>Demo</a>
+                    </div>
+                </li>
+            </ul>
         </section>
-        <style jsx>{`
-        .projects-container {width: 100%;height: 70vh; margin: 24px auto; padding: 12px;}
-        .coming-soon {display: flex; justify-content: center; align-items: center; font-size: 32px;}
-        `}</style>
-        </>
     );
 }
