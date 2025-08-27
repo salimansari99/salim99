1ac6e9420a65549ef99d434aa53d7014f2f6502a
Salim Ansari <salim.ansari@nw18.com>
2025-08-27 16:37:22 +0530

    add new project

--- src/app/components/Projects/index.tsx
+++ src/app/components/Projects/index.tsx
@@ -53,6 +53,30 @@ export default function Projects() {
                         <a href="https://todo-pilot.vercel.app/" className={styles.demoLink}>Demo</a>
                     </div>
                 </li>
+                <li className={styles.projectItem}>
+                    <div className={styles.projectTop}>
+                        {/* <img className={styles.suitcaseIc} src="/images/briefcase.png" alt="" /> */}
+                        <h4 className={styles.projectName}>The InsightPress</h4>
+                    </div>
+                    <p className={styles.projectDesc}>
+                        The InsightPress – A Modern Media Website for News and Articles.
+                    </p>
+                    <ul className={styles.descList}>
+                        <li>Built a responsive and user-friendly news platform using React/Next.js with modern UI components for browsing articles by categories.</li>
+                        <li>Implemented user authentication and subscription flows (login, signup, and membership features) to enhance personalization and access control.</li>
+                        <li>Designed and optimized article listing, search, and category-based filtering for seamless content discovery and improved user experience across devices.</li>
+                    </ul>
+                    <ul className={styles.technologies}>
+                        <li className={styles.techItem}>ReactJS</li>
+                        <li className={styles.techItem}>NextJS</li>
+                        <li className={styles.techItem}>JavaScript</li>
+                        <li className={styles.techItem}>Context API</li>
+                    </ul>
+                    <div className={styles.demo}>
+                        <img className={styles.linkIc} src="/images/link.png" alt="Link" />
+                        <a href="https://todo-pilot.vercel.app/" className={styles.demoLink}>Demo</a>
+                    </div>
+                </li>
             </ul>
         </section>
     );
