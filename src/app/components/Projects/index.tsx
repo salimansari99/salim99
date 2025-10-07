17a6604b8790c069efa7f2479b12c28f05c08ace
Salim Ansari <salim.ansari@nw18.com>
2025-10-08 00:01:24 +0530

    update project details and add new project "Byte Space"

--- src/app/components/Projects/index.tsx
+++ src/app/components/Projects/index.tsx
@@ -9,29 +9,6 @@ export default function Projects() {
                 <li className={styles.projectItem}>
                     <div className={styles.projectTop}>
                         {/* <img className={styles.suitcaseIc} src="/images/briefcase.png" alt="" /> */}
-                        <h4 className={styles.projectName}>The Thought Waves</h4>
-                    </div>
-                    <p className={styles.projectDesc}>
-                        Thought Waves – A Modern Blogging Platform for Insightful Minds
-                    </p>
-                    <ul className={styles.descList}>
-                        <li>✍️ Showcases well-researched articles on AI, technology, travel, and creativity.</li>
-                        <li>📱 Optimized for all devices with a clean and engaging UI.</li>
-                        <li>🧩 Features include blog listings, individual post pages, and popular posts section.</li>
-                    </ul>
-                    <ul className={styles.technologies}>
-                        <li className={styles.techItem}>ReactJS</li>
-                        <li className={styles.techItem}>NextJS</li>
-                        <li className={styles.techItem}>TypeScript</li>
-                    </ul>
-                    <div className={styles.demo}>
-                        <img className={styles.linkIc} src="/images/link.png" alt="Link" />
-                        <a href="https://thought-waves.vercel.app/" className={styles.demoLink}>Demo</a>
-                    </div>
-                </li>
-                <li className={styles.projectItem}>
-                    <div className={styles.projectTop}>
-                        {/* <img className={styles.suitcaseIc} src="/images/briefcase.png" alt="" /> */}
                         <h4 className={styles.projectName}>TodoPilot</h4>
                     </div>
                     <p className={styles.projectDesc}>
@@ -75,6 +52,52 @@ export default function Projects() {
                     <div className={styles.demo}>
                         <img className={styles.linkIc} src="/images/link.png" alt="Link" />
                         <a href="https://insightpress-media-frontend.vercel.app/" className={styles.demoLink}>Demo</a>
+                    </div>
+                </li>
+                <li className={styles.projectItem}>
+                    <div className={styles.projectTop}>
+                        {/* <img className={styles.suitcaseIc} src="/images/briefcase.png" alt="" /> */}
+                        <h4 className={styles.projectName}>The Thought Waves</h4>
+                    </div>
+                    <p className={styles.projectDesc}>
+                        Thought Waves – A Modern Blogging Platform for Insightful Minds
+                    </p>
+                    <ul className={styles.descList}>
+                        <li>✍️ Showcases well-researched articles on AI, technology, travel, and creativity.</li>
+                        <li>📱 Optimized for all devices with a clean and engaging UI.</li>
+                        <li>🧩 Features include blog listings, individual post pages, and popular posts section.</li>
+                    </ul>
+                    <ul className={styles.technologies}>
+                        <li className={styles.techItem}>ReactJS</li>
+                        <li className={styles.techItem}>NextJS</li>
+                        <li className={styles.techItem}>TypeScript</li>
+                    </ul>
+                    <div className={styles.demo}>
+                        <img className={styles.linkIc} src="/images/link.png" alt="Link" />
+                        <a href="https://thought-waves.vercel.app/" className={styles.demoLink}>Demo</a>
+                    </div>
+                </li>
+                <li className={styles.projectItem}>
+                    <div className={styles.projectTop}>
+                        {/* <img className={styles.suitcaseIc} src="/images/briefcase.png" alt="" /> */}
+                        <h4 className={styles.projectName}>Byte Space</h4>
+                    </div>
+                    <p className={styles.projectDesc}>
+                        ByteSpace – bite-sized yet impactful tech content.
+                    </p>
+                    <ul className={styles.descList}>
+                        <li>✍️ Integrated secure login/signup using Google OAuth for a seamless and quick onboarding experience.</li>
+                        <li>📱 	Includes an interactive contact form and links to social media profiles.</li>
+                        <li>🧩 Modular page structure for easy addition of new sections like community discussions or podcast episodes in the future.</li>
+                    </ul>
+                    <ul className={styles.technologies}>
+                        <li className={styles.techItem}> Next.js (React Framework)</li>
+                        <li className={styles.techItem}>NextAuth.js with Google OAuth</li>
+                        <li className={styles.techItem}>Zustand (State Management Library)</li>
+                    </ul>
+                    <div className={styles.demo}>
+                        <img className={styles.linkIc} src="/images/link.png" alt="Link" />
+                        <a href="https://bytespace-omega.vercel.app/" className={styles.demoLink}>Demo</a>
                     </div>
                 </li>
             </ul>
